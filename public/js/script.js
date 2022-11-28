9ee57b1f936384cac87bdeee15da767d95d4f103
Gary Blackwood <gary@garyblackwood.co.uk>
2022-11-28 11:34:25 +0000

    refactor: add Rebecca's own creation

---
+++ public/js/script.js
@@ -0,0 +1,34 @@
+var isOpen = false;
+
+function menuToggle() {
+    isOpen = !isOpen;
+
+    var x = document.getElementById('mynavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+    var y = document.getElementById('header');
+    if (y.className === '') {
+        y.className += 'responsive';
+    } else {
+        y.className = '';
+    }
+    var icon = document.querySelector('.icon > i');
+    if (icon.className === 'fa-solid fa-bars') {
+        icon.className = 'fa-solid fa-x';
+    } else {
+        icon.className = 'fa-solid fa-bars';
+    }
+
+    if (isOpen) {
+        $("#mynavtoggle").hide().fadeIn({ duration: 1000 });
+    }
+}
+
+addEventListener("resize", (event) => {
+    if (window.innerWidth > 640 && isOpen) {
+        menuToggle();
+    }
+});
